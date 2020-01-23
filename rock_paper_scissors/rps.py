77e57fecb810fa7fb11ff571331b781f39c6f1a2
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-01-23 17:44:44 -0500

    finished an iterative way of completing the rock paper scissors problem

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,40 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  options = ['rock', 'paper', 'scissors']
+  possible_chances = []
+
+  if n == 0:
+      return [[]]
+  elif n == 1:
+      return [[options[0]], [options[1]], [options[2]]]
+  if n == 2:
+      for first_option in options:
+        for second_option in options:
+          comb = [first_option, second_option]
+          possible_chances.append(comb)
+  if n == 3:
+      for first_option in options:
+        for second_option in options:
+          for third_option in options:
+            comb = [first_option, second_option, third_option]
+            possible_chances.append(comb)
+  if n == 4:
+      for first_option in options:
+        for second_option in options:
+          for third_option in options:
+            for fourth_option in options:
+                comb = [first_option, second_option, third_option, fourth_option]
+                possible_chances.append(comb)
+  if n == 5:
+      for first_option in options:
+        for second_option in options:
+          for third_option in options:
+            for fourth_option in options:
+              for fifth_option in options:
+                  comb = [first_option, second_option, third_option, fourth_option, fifth_option]
+                  possible_chances.append(comb)
+  return possible_chances
 
 
 if __name__ == "__main__":
